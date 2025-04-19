d80a19bbb86c7e58e9f7ce3ddda6c4a5a285d6ca
ARtbibaOfficial <giovanni.basso@icloud.com>
2025-04-19 17:18:57 +0200

    first code

---
+++ ca1d/ca1d.ino
@@ -0,0 +1,107 @@
+#include <DMD2.h>
+#include <SPI.h>
+
+#include <DMXSerial.h>
+
+
+#define DISPLAYS_WIDE 2
+#define DISPLAYS_HIGH 1
+
+SoftDMD dmd(DISPLAYS_WIDE, DISPLAYS_HIGH);
+DMDFrame frame(64, 16);
+
+
+#define ROW_LENGTH 64
+
+uint8_t CURRENT_RULE = 30;
+uint8_t DATA[ROW_LENGTH * 2];
+
+uint8_t* current_cell_states;
+uint8_t* next_cell_states;
+uint8_t* temp;
+
+int startChannel = 1;
+
+int speed = 255;
+
+
+int stateBasedOnNeighbors(int rule, int left, int center, int right) {
+  return(rule >> (left << 2 | center << 1 | right)) & 1;
+}
+
+void setStartingValues(bool rand_start) {
+  if(rand_start) {
+    for(int i = 0; i < ROW_LENGTH; i++)
+      current_cell_states[i] = rand()%2;
+  } else {
+    for(int i = 0; i < ROW_LENGTH; i++)
+      current_cell_states[i] = 0;
+    current_cell_states[ROW_LENGTH / 2] = 1;
+  }
+}
+
+void setup() {
+  DMXSerial.init(DMXReceiver);
+
+  dmd.setBrightness(100);
+  dmd.begin();
+
+  current_cell_states = DATA;
+  next_cell_states = DATA + ROW_LENGTH;
+
+  current_cell_states[ROW_LENGTH / 2] = 1;
+
+  DMXSerial.write(startChannel, 45);
+  DMXSerial.write(startChannel + 1, 0);
+  DMXSerial.write(startChannel + 2, 0);
+}
+
+uint8_t row_to_update = 0;
+
+uint8_t restart_timer = 0;
+
+void loop() {
+  unsigned long lastPacket = DMXSerial.noDataSince();
+  
+  speed = (DMXSerial.read(startChannel) + 5) * 5;
+  uint8_t ch2_read = DMXSerial.read(startChannel + 1);
+  if(ch2_read < 64)
+    CURRENT_RULE = 30;
+  else if(ch2_read >= 64 && ch2_read < 128)
+    CURRENT_RULE = 57;
+  else if(ch2_read >= 128 && ch2_read < 192)
+    CURRENT_RULE = 73;
+  else
+    CURRENT_RULE = 90;
+  
+  uint8_t ch3_read = DMXSerial.read(startChannel + 2);
+  if((ch3_read > 220) && (ch3_read < 240))
+    setStartingValues(true);
+  if((ch3_read > 240) && (ch3_read < 250))
+    setStartingValues(false);
+
+  if(row_to_update == 15)
+    frame.scrollY(-1);
+
+  for(int i = 0; i < ROW_LENGTH; i++)
+    frame.setPixel(i, row_to_update, (current_cell_states[i]? GRAPHICS_ON : GRAPHICS_OFF));
+  
+  for(int i = 0; i < ROW_LENGTH; i++) {
+    int left = current_cell_states[((i - 1) + ROW_LENGTH) % ROW_LENGTH];
+    int curr = current_cell_states[i];
+    int right = current_cell_states[(i + 1) % ROW_LENGTH];
+		
+    next_cell_states[i] = stateBasedOnNeighbors(CURRENT_RULE, left, curr, right);
+  }
+
+  dmd.copyFrame(frame, 0, 0);
+
+  temp = current_cell_states;
+  current_cell_states = next_cell_states;
+  next_cell_states = temp;
+
+  if(row_to_update < 15)
+    row_to_update++;
+
+  delay(speed);
+}
